9538dbbe28473497b3e95c39c4b7032422f8a8a5
edujeeho <edujeeho@gmail.com>
2024-01-04 15:59:48 +0900

    init

---
+++ src/main/java/com/example/crud/StudentController.java
@@ -0,0 +1,98 @@
+package com.example.crud;
+
+import com.example.crud.model.StudentDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+@Controller
+public class StudentController {
+
+    private StudentService service;
+
+    public StudentController(StudentService studentService) {
+        this.service = studentService;
+    }
+
+    // /create-view로 요청이 왔을때
+    // create.html을 반환하는 메서드
+    // @RequestMapping("/create-view")
+    @GetMapping("/create-view")
+    public String createView() {
+        return "create";
+    }
+
+    // /create로 이름과 이메일 데이터를
+    // 보내는 요청을 받는 메서드
+    @PostMapping("/create")
+    public String create(
+            @RequestParam("name")
+            String name,
+            @RequestParam("email")
+            String email
+    ) {
+        service.createStudent(name, email);
+        return "redirect:/home";
+    }
+
+    // /home으로 요청을 받으면
+    // home.html에 studentList를 포함해 반환하는 메서드
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("studentList", service.readStudentAll());
+        return "home";
+    }
+
+    // /read로 요청을 받으면
+    // read.html에 student 정보를 포함해 반환하는 메서드
+    // Mapping에 {}를 넣으면 그 안에 들어가 있는 데이터를
+    // 매개변수에 할당해 줄 수 있다.
+    @GetMapping("/read/{id}")
+    public String readOne(
+            @PathVariable("id")
+            Long id,
+            Model model
+    ) {
+        StudentDto dto = service.readStudent(id);
+        model.addAttribute("student", dto);
+        return "read";
+    }
+
+    // /update-view/{id}로 요청을 받으면
+    // update.html에 student 정보를 포함해 반환하는 메서드
+    @GetMapping("/update-view/{id}")
+    public String updateView(
+            @PathVariable("id")
+            Long id,
+            Model model
+    ) {
+        StudentDto dto = service.readStudent(id);
+        model.addAttribute("student", dto);
+        return "update";
+    }
+
+    // 여기
+    @PostMapping("/update/{id}")
+    public String update(
+            @PathVariable("id")
+            Long id,
+            @RequestParam("name")
+            String name,
+            @RequestParam("email")
+            String email
+    ) {
+        StudentDto dto = service.updateStudent(id, name, email);
+        return String.format("redirect:/read/%s", dto.getId());
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        service.deleteStudent(id);
+        return "redirect:/home";
+    }
+}
---
+++ src/main/java/com/example/crud/model/StudentDto.java
@@ -0,0 +1,23 @@
+package com.example.crud.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Data Transfer Object
+// Value Object
+// Data Object
+// 학생의 데이터를 담기 위한 클래스이다.
+// Getter, Setter, EqualAndHashCode
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDto {
+    // 데이터베이스의 PK
+    private Long id;
+    // 이름 정보
+    private String name;
+    // 이메일
+    private String email;
+}
